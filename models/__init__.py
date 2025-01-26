44917bbdff54a43a372e726b9824c97f27701fcf
Sergei Bykov <ss.bykoff@gmail.com>
2025-01-26 18:19:16 +0300

    добавлена модель User

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,4 @@
 from .day_info import DayInfo, YelamModel, HaircuttingModel, LaModel, ElementModel, ArchModel
 from .user import User
+
+__all__ = ["DayInfo", "YelamModel", "HaircuttingModel", "LaModel", "ElementModel", "ArchModel", "User"]
--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
-from sqlalchemy import String, DateTime, func
+from sqlalchemy import DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database.database import Base
@@ -9,6 +9,7 @@ from database.database import Base
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
     __tablename__ = "users"
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)  # type: ignore[assignment]
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), onupdate=func.now())
