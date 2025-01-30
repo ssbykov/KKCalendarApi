a9ecc0f113289761b60c4606743f46f152bc2a02
Sergei Bykov <ss.bykoff@gmail.com>
2025-01-30 19:18:05 +0300

    добавил параметр __abstract__

--- database/database.py
+++ database/database.py
@@ -4,5 +4,7 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
+    __abstract__ = True
+
     init_data: list[dict[str, Any]] = []
     id: Mapped[int] = mapped_column(primary_key=True, index=True, unique=True)
