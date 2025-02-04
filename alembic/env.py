aa92568dc983715bd07409a158162013a264e04c
Sergei Bykov <ss.bykoff@gmail.com>
2025-02-04 14:22:11 +0300

    сбросил alembic

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,9 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from core.config import settings
+from database import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -20,9 +23,6 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from database.models import Base
-from core.config import settings
-
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
