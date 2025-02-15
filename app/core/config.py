d45cb5ab9061f7c92e94e4f64626d31bc9b74888
Sergei Bykov <ss.bykoff@gmail.com>
2025-02-15 11:39:28 +0300

    добавил параметр naming_convention для alembic

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 import os
 
 from pydantic import BaseModel, PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -25,10 +25,22 @@ class DbSettings(BaseModel):
         f":{os.getenv('DB_PORT')}"
         f"/{os.getenv('DB_BASE')}"
     )
-    echo: bool = False
+    echo: bool = True
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=False,
+        env_nested_delimiter="__",
+    )
     run: RunConfig = RunConfig()
     api_v1_prefix: str = "/api/v1"
     db: DbSettings = DbSettings()
--- app/database/models/base.py
+++ app/database/models/base.py
@@ -1,10 +1,15 @@
 from typing import Any
 
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+
+from core import settings
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
 
     init_data: list[dict[str, Any]] = []
     id: Mapped[int] = mapped_column(primary_key=True, index=True, unique=True)
