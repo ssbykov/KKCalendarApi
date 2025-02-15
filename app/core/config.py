78b811d48fbd50ed1a1206f2cc2da2a969f956c9
Sergei Bykov <ss.bykoff@gmail.com>
2025-02-15 13:35:45 +0300

    убрал model_config

--- app/core/config.py
+++ app/core/config.py
@@ -46,12 +46,12 @@ class DbSettings(BaseModel):
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        case_sensitive=False,
-        env_nested_delimiter="__",
-        env_prefix="APP_CONFIG__",
-    )
+    # model_config = SettingsConfigDict(
+    #     env_file=".env",
+    #     case_sensitive=False,
+    #     env_nested_delimiter="__",
+    #     env_prefix="APP_CONFIG__",
+    # )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     sql_admin: SqlAdminSettings = SqlAdminSettings()
