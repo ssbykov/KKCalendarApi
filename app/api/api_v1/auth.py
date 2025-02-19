8adc13a901f7a9338ad5853d8205bdc95941f8b9
Sergei Bykov <ss.bykoff@gmail.com>
2025-02-19 19:21:31 +0300

    добавлены роуты для верификации

--- app/api/api_v1/auth.py
+++ app/api/api_v1/auth.py
@@ -11,7 +11,16 @@ router = APIRouter(
 )
 
 # login, logout
-router.include_router(router=fastapi_users.get_auth_router(authentication_backend))
+router.include_router(
+    router=fastapi_users.get_auth_router(authentication_backend),
+)
 
 # register
-router.include_router(fastapi_users.get_register_router(UserRead, UserCreate))
+router.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+)
+
+# verify
+router.include_router(
+    router=fastapi_users.get_verify_router(UserRead),
+)
