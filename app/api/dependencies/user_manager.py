5620e36eb4d2b9fe57b28409de1096b91ad704aa
Sergei Bykov <ss.bykoff@gmail.com>
2025-02-22 11:41:33 +0300

    исправлены ошибки с типизацией

--- app/api/dependencies/user_manager.py
+++ app/api/dependencies/user_manager.py
@@ -1,14 +1,16 @@
-from typing import AsyncGenerator, Any
+from typing import AsyncGenerator, TYPE_CHECKING
 
 from fastapi import Depends
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 
 from api.dependencies.users import get_user_db
 from core.auth.user_manager import UserManager
-from database.models import User
+
+if TYPE_CHECKING:
+    from database.models import User
 
 
 async def get_user_manager(
-    user_db: SQLAlchemyUserDatabase = Depends(get_user_db),
-):
+    user_db: SQLAlchemyUserDatabase["User", int] = Depends(get_user_db),
+) -> AsyncGenerator[UserManager, None]:
     yield UserManager(user_db)
