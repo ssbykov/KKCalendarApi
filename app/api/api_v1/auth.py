b597c7f819cae8eb77201d8bae836f73dfac4886
Sergei Bykov <ss.bykoff@gmail.com>
2025-02-22 18:42:30 +0300

    добавлен маршрут с get запросом для верификации нового пользователя

--- app/api/api_v1/auth.py
+++ app/api/api_v1/auth.py
@@ -1,14 +1,30 @@
 from fastapi import APIRouter
+from fastapi_users.exceptions import InvalidVerifyToken, UserAlreadyVerified
 
 from api.dependencies.backend import authentication_backend
+from api.dependencies.user_manager_helper import user_manager_helper
+from core.config import settings
 from database.schemas.user import UserRead, UserCreate
 from .fastapi_users import fastapi_users
-from core.config import settings
 
 router = APIRouter(
     prefix=settings.api.v1.auth,
     tags=["Auth"],
 )
+
+
+@router.get("/verify/")
+async def verify_user(token: str):
+    try:
+        user = await user_manager_helper.verify(token=token)
+        return f"Пользователь {user.email} верифицирован."
+    except InvalidVerifyToken as e:
+        print(e)
+        return "Невалидный токен"
+    except UserAlreadyVerified as e:
+        print(e)
+        return "Пользователь уже верифицирован"
+
 
 # login, logout
 router.include_router(
