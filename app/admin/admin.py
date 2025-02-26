98aecb5dd41eb9c64fd7d54c2bc4425439174ada
Sergei Bykov <ss.bykoff@gmail.com>
2025-02-26 14:04:46 +0300

    добавлена кастомизация метода login

--- app/admin/admin.py
+++ app/admin/admin.py
@@ -1,15 +1,17 @@
 from typing import Any
 
 from sqladmin import Admin
+from starlette.requests import Request
+from starlette.responses import Response, RedirectResponse
 
-from .admin_auth import sql_admin_authentication_backend
-from .model_views import EventAdmin, DayInfoAdmin
 from core import settings
 from database import db_helper
+from .admin_auth import sql_admin_authentication_backend, AdminAuth
+from .model_views import EventAdmin, DayInfoAdmin
 
 
 def init_admin(app: Any) -> None:
-    admin = Admin(
+    admin = NewAdmin(
         app,
         db_helper.engine,
         templates_dir=settings.sql_admin.templates,
@@ -17,3 +19,22 @@ def init_admin(app: Any) -> None:
     )
     admin.add_view(DayInfoAdmin)
     admin.add_view(EventAdmin)
+
+
+class NewAdmin(Admin):
+    async def login(self, request: Request) -> Response:
+        assert isinstance(self.authentication_backend, AdminAuth)
+
+        context: dict[str, str | None] = {}
+        if request.method == "GET":
+            return await self.templates.TemplateResponse(request, "sqladmin/login.html")
+
+        response = await self.authentication_backend.login_with_info(request)
+        if not response.is_ok:
+            context["error"] = response.error
+            context["message"] = response.message
+            return await self.templates.TemplateResponse(
+                request, "sqladmin/login.html", context, status_code=400
+            )
+
+        return RedirectResponse(request.url_for("admin:index"), status_code=302)
