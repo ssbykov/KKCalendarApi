ae1a1364d88a689804fcabc48b2aad20617fc998
Sergei Bykov <ss.bykoff@gmail.com>
2025-03-04 00:02:41 +0300

    заменил заголовки на русские

--- app/admin/model_views.py
+++ app/admin/model_views.py
@@ -15,6 +15,37 @@ class EventAdmin(ActionNextBackMixin, ModelView, model=Event):
     repo_type = EventRepository
     name_plural = "События"
     name = "Информация по событию"
+    form_create_rules = [
+        "days",
+        "name",
+        "en_name",
+        "en_text",
+        "ru_name",
+        "ru_text",
+        "link",
+    ]
+    form_edit_rules = [
+        "days",
+        "name",
+        "en_name",
+        "en_text",
+        "ru_name",
+        "ru_text",
+        "link",
+    ]
+
+    column_labels = {
+        "days": "Даты события",
+        "name": "Название",
+        "user": "Пользователь",
+        "moon_day": "Лунные дни",
+        "en_name": "Заголовок на английском",
+        "en_text": "Описание на английском",
+        "ru_name": "Заголовок на русском",
+        "ru_text": "Описание на русском",
+        "link": "Ссылка на событие",
+    }
+
     column_list = [
         Event.name,
         Event.en_name,
@@ -29,9 +60,11 @@ class EventAdmin(ActionNextBackMixin, ModelView, model=Event):
         Event.en_text: lambda model, attribute: Markup(
             formater(getattr(model, "en_text", ""))
         ),
-        Event.link: lambda model, attribute: Markup(
+        Event.link: lambda model, attribute: getattr(model, "link", None)
+        and Markup(
             f'<a href="{getattr(model, "link", "#")}" target="_blank">{getattr(model, "link", "No URL")}</a>'
-        ),
+        )
+        or "",
     }
 
     column_details_exclude_list = [Event.id, Event.user_id, Event.is_mutable]
@@ -44,12 +77,42 @@ class EventAdmin(ActionNextBackMixin, ModelView, model=Event):
         stmt: Select[Any] = select(func.count(self.pk_columns[0]))
         return self.get_query(request, stmt)
 
-    @staticmethod
-    def get_query(request: Request, stmt: Select[Any]) -> Select[Any]:
-        user = request.session.get("user")
-        if isinstance(user, dict) and not user.get("is_superuser"):
+    def form_edit_query(self, request: Request) -> Select[Any]:
+        if self.get_user(request):
+            if "user" in self.form_edit_rules:
+                self.form_edit_rules.remove("user")
+        else:
+            self.form_edit_rules.append("user")
+        return super().form_edit_query(request)
+
+    def get_query(self, request: Request, stmt: Select[Any]) -> Select[Any]:
+        if user := self.get_user(request):
             stmt = stmt.filter(Event.user_id == user.get("id"))
         return stmt
+
+    async def on_model_change(
+        self,
+        data: dict[str, Any],
+        model: Any,
+        is_created: bool,
+        request: Request,
+    ) -> None:
+        data["link"] = self.ensure_http_prefix(data["link"])
+        if user := self.get_user(request):
+            data["user_id"] = user.get("id")
+
+    @staticmethod
+    def get_user(request: Request) -> Any | None:
+        user = request.session.get("user")
+        if isinstance(user, dict) and not user.get("is_superuser"):
+            return user
+        return None
+
+    @staticmethod
+    def ensure_http_prefix(url: str) -> str:
+        if url and not (url.startswith("http://") or url.startswith("https://")):
+            return f"http://{url}"
+        return url
 
 
 def formater(column: str) -> str:
