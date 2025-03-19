e56f9a0c76338cba832e14f2501ee5c513f22cef
Sergei Bykov <ss.bykoff@gmail.com>
2025-03-19 12:35:45 +0300

    переопределенный ImageType перенес в один модуль

--- app/core/custom_types.py
+++ app/core/custom_types.py
@@ -1,7 +1,19 @@
-from typing import Any
+import logging
+from pathlib import Path
+from typing import Any, Optional
 
-from fastapi_storages.integrations.sqlalchemy import ImageType as _ImageType
 from fastapi_storages import FileSystemStorage
+from fastapi_storages.integrations.sqlalchemy import ImageType as _ImageType
+from sqlalchemy.engine.interfaces import Dialect
+
+try:
+    from PIL import Image, UnidentifiedImageError
+
+    PIL = True
+except ImportError:  # pragma: no cover
+    PIL = False
+
+from fastapi_storages.base import StorageImage
 
 from core import settings
 
@@ -13,3 +25,29 @@ class ImageType(_ImageType):
             *args,
             **kwargs,
         )
+
+    def process_result_value(
+        self, value: Any, dialect: Dialect
+    ) -> Optional[StorageImage]:
+        if value is None:
+            return value
+
+        file_path = self.storage.get_path(value)
+        if not Path(file_path).exists():
+            logging.error(f"Файл {file_path} не найден.")
+            return None
+
+        try:
+            with Image.open(file_path) as image:
+                return StorageImage(
+                    name=value,
+                    storage=self.storage,
+                    height=image.height,
+                    width=image.width,
+                )
+        except UnidentifiedImageError:
+            logging.error(f"Файл {file_path} не является изображением.")
+            return None
+        except Exception as e:
+            logging.error(f"Ошибка при обработке изображения: {e}")
+            return None
