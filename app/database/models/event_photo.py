6cdb9458b04ebee25d23893d8d131cbd6befa441
Sergei Bykov <ss.bykoff@gmail.com>
2025-03-19 12:36:59 +0300

    переопределенный ImageType перенес в один модуль

--- app/database/models/event_photo.py
+++ app/database/models/event_photo.py
@@ -1,18 +1,15 @@
-from fastapi_storages import FileSystemStorage
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
-from core import settings
+from core.custom_types import ImageType
 from . import BaseWithId
-from database.custom_image_type import CustomImageType
 
 
 class EventPhoto(BaseWithId):
-    storage = FileSystemStorage(path=settings.image_storage.full_path)
     __tablename__ = "event_photos"
     name: Mapped[str] = mapped_column(String(30), nullable=False)
     photo_data: Mapped[str] = mapped_column(
-        CustomImageType(storage=storage),
+        ImageType(),
         nullable=False,
     )
 
