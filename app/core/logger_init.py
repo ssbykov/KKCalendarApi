3c48d9e1062cd42c36dccd563852b34fc8c90d8a
Sergei Bykov <ss.bykoff@gmail.com>
2025-03-25 12:22:51 +0300

    добавлен входной параметр log_file

--- app/core/logger_init.py
+++ app/core/logger_init.py
@@ -4,9 +4,8 @@ from logging.handlers import RotatingFileHandler
 from core import settings
 
 
-def init_logger() -> None:
+def init_logger(log_file: str = settings.logger.filename) -> None:
     log_level = settings.logger.log_level
-    log_file = settings.logger.filename
     numeric_level = getattr(logging, log_level.upper(), logging.ERROR)
     handler = RotatingFileHandler(
         log_file, maxBytes=1 * 1024 * 1024, backupCount=1  # 5 MB, 3 backup files
