3725c4312e530ef288342b02b5303b2259aa481e
Sergei Bykov <ss.bykoff@gmail.com>
2025-05-14 12:33:40 +0300

    добавил ограничение один к одному на фото ламы

--- app/database/models/lama.py
+++ app/database/models/lama.py
@@ -8,8 +8,12 @@ class Lama(BaseWithId):
     __tablename__ = "lamas"
     name: Mapped[str] = mapped_column(String(200), nullable=False, unique=True)
     description: Mapped[str] = mapped_column(Text, nullable=True)
-    photo_id: Mapped[int] = mapped_column(ForeignKey("event_photos.id"), nullable=True)
-    photo = relationship("LamaPhoto", backref=backref("lama"))
+    photo_id: Mapped[int] = mapped_column(
+        ForeignKey("event_photos.id"), nullable=True, unique=True
+    )
+    photo = relationship(
+        "EventPhoto", uselist=False, back_populates=None, viewonly=True
+    )
 
     def __str__(self) -> str:
         return self.name
