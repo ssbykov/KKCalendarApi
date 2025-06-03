5722535e3723bfa8311c7e721d807556785ed1ff
Sergei Bykov <ss.bykoff@gmail.com>
2025-06-03 11:39:46 +0300

    изменил функцию check_job_status

--- app/celery_worker.py
+++ app/celery_worker.py
@@ -1,5 +1,5 @@
 import redis
-from celery import Celery  # type: ignore
+from celery import Celery, Task  # type: ignore
 from celery.result import AsyncResult  # type: ignore
 
 from app.core import settings
@@ -14,12 +14,14 @@ celery_app = Celery(
 )
 
 celery_app.autodiscover_tasks(["app.tasks"])
-import app.tasks
 
 
 def check_job_status(name: str) -> AsyncResult | None:
     task_id = redis_client.get(name)
     if not task_id:
         return None
+    task = AsyncResult(task_id)
+    if task.status == "FAILURE":
+        redis_client.delete(name)
 
-    return AsyncResult(task_id)
+    return task
