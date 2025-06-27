0c1c59610cbe1b85eda46216ee4825e287ea54bd
Sergei Bykov <ss.bykoff@gmail.com>
2025-06-27 14:47:12 +0300

    исправлен путь

--- app/api/api_v1/quotes.py
+++ app/api/api_v1/quotes.py
@@ -1,22 +1,22 @@
-from typing import Sequence, Annotated
+from typing import Annotated
 
 from fastapi import APIRouter, Depends
 
 from app.core import settings
-from app.database.crud.quotes import QuoteRepository, get_quote_repository
 from app.database import Quote
+from app.database.crud.quotes import QuoteRepository, get_quote_repository
 from app.database.schemas.quote import QuoteSchema
 
 router = APIRouter(
     tags=["Quotes"],
+    prefix=settings.api.v1.quotes,
 )
 router.include_router(
     router,
-    prefix=settings.api.v1.quotes,
 )
 
 
-@router.post("/quote", response_model=QuoteSchema)
+@router.post("/", response_model=QuoteSchema)
 async def get_random_quote(
     excluded_ids: list[int],
     repo: Annotated[QuoteRepository, Depends(get_quote_repository)],
