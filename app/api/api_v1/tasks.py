50151c75dc1637556699496154d3d7b1e5c3c90d
Sergei Bykov <ss.bykoff@gmail.com>
2025-07-04 19:36:22 +0300

    доступ к задачам только суперюзеру

--- app/api/api_v1/tasks.py
+++ app/api/api_v1/tasks.py
@@ -2,9 +2,11 @@ from typing import Annotated, Sequence, List
 
 from fastapi import APIRouter, Depends
 
+from api.api_v1.fastapi_users import current_super_user
 from app.core import settings
 from app.database.crud.tasks import TaskRepository, get_task_repository
 from database import TaskScheduler
+from database.models import User
 from database.schemas.task import TaskSchema
 
 router = APIRouter(
@@ -19,6 +21,7 @@ router.include_router(
 @router.get("/", response_model=List[TaskSchema])
 async def get_all(
     repo: Annotated[TaskRepository, Depends(get_task_repository)],
+    user: Annotated[User, Depends(current_super_user)],
 ) -> Sequence[TaskScheduler] | str:
     try:
         return await repo.get_tasks()
