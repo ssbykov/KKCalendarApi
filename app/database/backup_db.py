4459601c1c196b82bec8dfe41fcf7d8078fb9007
Sergei Bykov <ss.bykoff@gmail.com>
2025-10-17 18:12:07 +0300

    исправлен импорт

--- app/database/backup_db.py
+++ app/database/backup_db.py
@@ -8,8 +8,8 @@ from typing import TYPE_CHECKING
 
 from app.core import settings
 from app.database import db_helper
-from database.crud.yandex_tokens import YandexTokensRepository
-from database.yandex_disk import create_yadisk_instance
+from app.database.crud.yandex_tokens import YandexTokensRepository
+from app.database.yandex_disk import create_yadisk_instance
 
 if TYPE_CHECKING:
     from app.core.config import DbSettings
